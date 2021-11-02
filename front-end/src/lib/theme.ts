90a88de0c7ca7f4bb0dbd0477d40e2ddf0c0977e
flu0r1ne <flu0r1ne@flu0r1ne.net>
2021-11-01 22:22:52 -0500

    Add basic markup for upload page

--- front-end/src/lib/theme.ts
+++ front-end/src/lib/theme.ts
@@ -1,11 +1,29 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
+import React from 'react';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    bg: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    bg?: PaletteOptions['primary'];
+  }
+
+  // interface PaletteColor {
+  //   white0?: string;
+  // }
+  // interface SimplePaletteColorOptions {
+  //   white0?: string;
+  // }
+}
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#556cd6'
     },
     secondary: {
       main: '#19857b',
@@ -13,6 +31,9 @@ const theme = createTheme({
     error: {
       main: red.A400,
     },
+    bg: {
+      main: '#ffffff'
+    }
   },
 });
 
--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/reset.scss';
+import '../styles/global.scss';
 import * as React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../lib/theme';
 import createEmotionCache from '../lib/createEmotionCache';
@@ -21,12 +21,12 @@ export default function MyApp(props: MyAppProps) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>My page</title>
+        <title>Code Analyzer</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
+        {/* <CssBaseline /> */}
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
