044e5f6bff3d6d9e73f03483758284dffdf9de7e
flu0r1ne <flu0r1ne@flu0r1ne.net>
2021-11-05 16:03:04 -0500

    Add clang-tidy dep to back-end and example

--- back-end/src/user/routes.py
+++ back-end/src/user/routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, jsonify
+import subprocess
+import tempfile
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
@@ -13,3 +15,37 @@ def ping_post():
     }
 
     return jsonify(data)
+
+# Create sample file for test analysis
+with open('/tmp/test.cpp', 'w') as file:
+	file.write("""
+	int main() {
+	  int i = 0, j = 0;
+	  while (i < 10) {
+		  ++j;
+	  }
+	}
+	""")
+
+@user.route('/tidy', methods=['GET'])
+def tidy():
+
+    with tempfile.NamedTemporaryFile('w+') as file:
+        CMD = [
+			'clang-tidy',
+			'/tmp/test.cpp',
+			# export to YAML file (the tmpfile above)
+			f'--export-fixes={file.name}', 
+			# disable all checks except for bugprone and cppcoreguidline
+			# we might want to change this in the future
+			# see: https://clang.llvm.org/extra/clang-tidy/checks/list.html
+			'-checks=-*,bugprone-*,cppcoreguidelines-*' 
+		]
+
+        subprocess.run(CMD)
+
+        suggestions = file.read()
+
+        print('suggestions', suggestions)
+
+        return suggestions
