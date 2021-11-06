120824c7d5d3a39f28b6e6de8584ebb8ae451036
flu0r1ne <flu0r1ne@flu0r1ne.net>
2021-11-06 18:58:50 -0500

    Remove unnecessary sample code
    A couple sample routes were committed for testing communication for
    development purposes.

--- back-end/src/user/routes.py
+++ back-end/src/user/routes.py
@@ -9,54 +9,9 @@ import json
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
-
 @user.route('/ping', methods=['GET'])
 def ping_pong():
     return 'pong'
-
-
-@user.route('/ping', methods=['POST'])
-def ping_post():
-    data = {
-        'hello': 'world'
-    }
-
-    return jsonify(data)
-
-# Create sample file for test analysis
-with open('/tmp/test.cpp', 'w') as file:
-	file.write("""
-	int main() {
-	  int i = 0, j = 0;
-	  while (i < 10) {
-		  ++j;
-	  }
-	}
-	""")
-
-@user.route('/tidy', methods=['GET'])
-def tidy():
-
-    with tempfile.NamedTemporaryFile('w+') as file:
-        CMD = [
-			'clang-tidy',
-			'/tmp/test.cpp',
-			# export to YAML file (the tmpfile above)
-			f'--export-fixes={file.name}', 
-			# disable all checks except for bugprone and cppcoreguidline
-			# we might want to change this in the future
-			# see: https://clang.llvm.org/extra/clang-tidy/checks/list.html
-			'-checks=-*,bugprone-*,cppcoreguidelines-*' 
-		]
-
-        subprocess.run(CMD)
-
-        suggestions = file.read()
-
-        print('suggestions', suggestions)
-
-        return suggestions
-
 
 UPLOAD_FOLDER = '/tmp'
 app = Flask(__name__)
