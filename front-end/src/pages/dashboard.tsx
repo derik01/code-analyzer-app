f80dc71c28b11d1d68ca85b9c50abe9bdce746b9
ankithpillay <ankithpillay@tamu.edu>
2021-11-07 14:19:00 -0600

    Display Diagnostic Message for one file

--- front-end/src/pages/dashboard.tsx
+++ front-end/src/pages/dashboard.tsx
@@ -5,20 +5,36 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import { Grid, Paper } from '@mui/material';
+import { Grid, Modal, Paper } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import CodeIcon from '@mui/icons-material/Code';
 import { Container, Stack } from '@mui/material';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 import { useDropzone } from 'react-dropzone';
-//import { useState } from "react";
+import { useState } from "react";
 //import { DropzoneArea } from 'material-ui-dropzone';
+
+let e 
+let name 
+let message 
 
 declare module '@mui/material/AppBar' {
     interface AppBarColorOverrides {
         bg: true;
     }
 }
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 function HeaderBar() {
     return (
@@ -61,7 +77,12 @@ function HeaderBar() {
 //   body: data
 // })
 const DropZone = () => {
-    //const [files, setFile] = useState<any>([]);
+
+    const [files, setFile] = useState<any[]>([]);
+    const [empty , setEmpty] = useState(true);
+    const [filename, setName] = useState("");
+    const [msg, setMsg] = useState(""); 
+    
     const onDrop = React.useCallback(acceptedFiles => {
         console.log(`Accepting Length: ${acceptedFiles.length}`);
         acceptedFiles.map(async file => {
@@ -75,7 +96,27 @@ const DropZone = () => {
             }).then(
                 res => res.json()
             ).then(
-                success => console.log(success)
+                success => {
+                    
+                   // const f = success;
+
+                    setFile(success);
+                    
+                    setEmpty(false);
+                    Object.keys(success).map((key) => (
+                        //console.log(key)
+                        setName(key)
+                    ));
+                    Object.values(success).map((m) => (
+                        setMsg(m.Diagnostics[0].DiagnosticMessage.Message)
+                   ));
+                   //trying out useContext() hook
+                   e = empty;
+                   name = filename;
+                   message = msg;
+                   //console.log(filename);
+
+                }
             ).catch(
                 err => console.log(err)
             );
@@ -96,57 +137,114 @@ const DropZone = () => {
         //     err => console.log(err)
         // );
 
-    }, []);
+    }, [files, empty, filename, msg]);
+    //console.log(files);
+    e = empty;
+    name = filename;
+    message = msg;
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
-    return (
-    <div
-        style={{
-            padding: '1em',
-        }}
-    >
-        <div
-            style={{
-                outline: '0.15rem dashed grey',
-                borderRadius: '5px',
-                padding: '6em 0'
-            }}
-
-            {...getRootProps()}
-        >
-            <input {...getInputProps()} />
-            <Stack
+    
+    //test.cpp -> Diagnostics[0] -> DiagnosticMessage -> Message
+    if(empty){
+        return (
+            <div
                 style={{
-                    width: '300px',
-                    margin: '0 auto'
+                    padding: '1em',
                 }}
             >
                 <div
                     style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center'
+                        outline: '0.15rem dashed grey',
+                        borderRadius: '5px',
+                        padding: '6em 0'
                     }}
+        
+                    {...getRootProps()}
                 >
-                    <FileUploadIcon
-                        color="primary"
-                        style={{fontSize: '4rem', padding: '0.1em'}}
-                    />
+                    <input {...getInputProps()} />
+                    <Stack
+                        style={{
+                            width: '300px',
+                            margin: '0 auto'
+                        }}
+                    >
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <FileUploadIcon
+                                color="primary"
+                                style={{fontSize: '4rem', padding: '0.1em'}}
+                            />
+                        </div>
+                        <span style={{marginBottom: '1.5em'}}>
+                        <Typography 
+                            variant="h5"
+                            textAlign='center'
+                        >
+                            Drag and drop files 
+                        </Typography>
+                        </span>
+                        <Button variant="contained" style={{display: 'inline'}}>Select Files</Button>
+                    </Stack>
                 </div>
-                <span style={{marginBottom: '1.5em'}}>
-                <Typography 
-                    variant="h5"
-                    textAlign='center'
+            </div>
+            );
+    }
+    else{
+        return (
+            <div
+                style={{
+                    padding: '1em',
+                }}
+            >
+                <div
+                    style={{
+                        outline: '0.15rem dashed grey',
+                        borderRadius: '5px',
+                        padding: '6em 0'
+                    }}
+        
+                    {...getRootProps()}
                 >
-                    Drag and drop files
-                </Typography>
-                </span>
-                <Button variant="contained" style={{display: 'inline'}}>Select Files</Button>
-            </Stack>
-        </div>
-    </div>
-    );
+                    <input {...getInputProps()} />
+                    <Stack
+                        style={{
+                            width: '300px',
+                            margin: '0 auto'
+                        }}
+                    >
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <FileUploadIcon
+                                color="primary"
+                                style={{fontSize: '4rem', padding: '0.1em'}}
+                            />
+                        </div>
+                        <span style={{marginBottom: '1.5em'}}>
+                        <Typography 
+                            variant="h5"
+                            textAlign='center'
+                        >
+                            Drag and drop files 
+                        </Typography>
+                        </span>
+                        <Button variant="contained" style={{display: 'inline'}}>Select Files</Button>
+                    </Stack>
+                </div>
+            </div>
+            );
+    }  
 }
 
 const UploadPannel = () => ( 
@@ -169,11 +267,45 @@ const UploadPannel = () => (
     </Box>
 );
 
+const ModalRec = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    //const e = React.useContext(isEmpty);
+    
+        return(
+            <div>
+                <Button onClick={handleOpen}>View Reccomendations</Button>
+                <Modal
+                    aria-labelledby="unstyled-modal-title"
+                    aria-describedby="unstyled-modal-description"
+                    open={open}
+                    onClose={handleClose}
+                >
+                <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                     Reccomendation for file: {name}
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    {message}
+                 </Typography>
+                </Box>
+            </Modal>
+            </div>
+                
+        );
+    
+};
+
+
+
 export default function Dashboard() {
+
     return (   
     <Box sx={{ flexGrow: 1 }}>
        <HeaderBar />
         <Container>
+            <ModalRec></ModalRec>
             <Paper
                 elevation={3}
             >
