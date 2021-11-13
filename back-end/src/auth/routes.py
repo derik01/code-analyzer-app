375950aeb2d8f4a1131f3132264b10edbf0839fe
Aref Sadeghi <a.sadeghi@tamu.edu>
2021-11-12 20:10:47 -0600

    update auth/routes.py

--- back-end/src/auth/routes.py
+++ back-end/src/auth/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
@@ -10,7 +11,12 @@ def ping_pong():
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
+def validate_user(username, email):
+    if username and email:
+        return True
+    else:
+        return False
+
 @app.route('/register', methods=['GET'])
-def register(name, lastName, username, password):
+def register(name, lastName, email, username, password):
     hash_pass = bcrypt.generate_password_hash(password)
-    
