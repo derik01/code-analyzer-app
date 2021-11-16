566727b73fbf6252fb7512df57d77ef4579d1f93
Aref Sadeghi <a.sadeghi@tamu.edu>
2021-11-16 11:49:34 -0600

    Updated authorizatoin routes

--- back-end/src/auth/routes.py
+++ back-end/src/auth/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, Flask
 import pymongo
 from flask_bcrypt import Bcrypt
+import re
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -37,46 +38,62 @@ def validate_user(username, password):
         return "No User"
 
 
-def register1(firstName, lastName, email, username, password):
-    if not user_exists(username, email):
-        if len(password) < 8:
-            print("Password must be at least 8 characters long")
-        else:
-            hash_pass = bcrypt.generate_password_hash(password).decode('utf-8')
-            userRecord = {
-                "firstName": firstName,
-                "lastName": lastName,
-                "email": email,
-                "userName": username,
-                "password": hash_pass
-            }
-            info.insert_one(userRecord)
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+
+def check(email):
+    if re.fullmatch(regex, email):
+        return True
     else:
-        print("User already exists")
+        return False
 
 
-def login1(username, password):
-    if validate_user(username, password) == "True":
-        print("You are logged in")
-    elif validate_user(username, password) == "False":
-        print("The username and password do not match")
-    else:
-        print("The username does not exist")
+#def register1(firstName, lastName, email, username, password):
+#    if not user_exists(username, email):
+#        if len(password) < 8:
+#            return "Short Password"
+#        elif not check(email):
+#            return "Invalid Email"
+#        else:
+#            hash_pass = bcrypt.generate_password_hash(password).decode('utf-8')
+#            userRecord = {
+#                "firstName": firstName,
+#                "lastName": lastName,
+#                "email": email,
+#                "userName": username,
+#                "password": hash_pass
+#            }
+#            info.insert_one(userRecord)
+#    else:
+#        print("User already exists")
 
 
-register1("Aref", "Sadeghi", "Aref@example.com", "ArefS", "password")
-register1("Alex", "Born", "Alex@example.com", "AlexB", "password2")
-register1("Ankith", "Pillay", "Ankith@example.com", "AnkithP", "password3")
-register1("Jamie", "Wist", "Jamie@example.com", "JamieW", "password4")
-register1("Derik", "Wang", "Wang@example.com", "DerikW", "password5")
-login1("JamieW", "password4")
-#info.delete_one({"userName": "AnkithP"})
+#def login1(username, password):
+#    if validate_user(username, password) == "True":
+#        print("You are logged in")
+#    elif validate_user(username, password) == "False":
+#        print("The username and password do not match")
+#    else:
+#        print("The username does not exist")
+
+
+#register1("Aref", "Sadeghi", "Aref@example.com", "ArefS", "password")
+#register1("Alex", "Born", "Alex@example.com", "AlexB", "password2")
+#register1("Ankith", "Pillay", "Ankith@example.com", "AnkithP", "password3")
+#register1("Jamie", "Wist", "Jamie@example.com", "JamieW", "password4")
+#register1("Derik", "Wang", "Wang@example.com", "DerikW", "password5")
+#login1("JamieW", "password4")
+
+# info.delete_one({"userName": "AnkithP"})
+
 
 @app.route('/register', methods=['POST'])
 def register(firstName, lastName, email, username, password):
     if not user_exists(username, email):
         if password.len() < 8:
-            print("Password must be at least 8 characters long")
+            return "Password Error"
+        elif not check(email):
+            return "Invalid Email"
         else:
             hash_pass = bcrypt.generate_password_hash(password).decode('utf-8')
             userRecord = {
@@ -87,13 +104,14 @@ def register(firstName, lastName, email, username, password):
                 "password": hash_pass
             }
             info.insert_one(userRecord)
+            return "Good"
 
 
 @app.route('/login', methods=['POST'])
 def login(username, password):
     if validate_user(username, password) == "True":
-        print("You are logged in")
+        return "Good"
     elif validate_user(username, password) == "False":
-        print("The username and password do not match")
+        return "U/P Mismatch"
     else:
-        print("The username does not exist")
+        return "No User"
